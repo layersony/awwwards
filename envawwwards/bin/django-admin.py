87a3f74174568e21ce7801a23fa132856ccf881f
layersony <sammaingi5@gmail.com>
2021-07-17 07:07:31 +0300

    initial commit

---
+++ envawwwards/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/layersony/Documents/Canvas_Moringa/Core/Python/django/Week_3/awwwards/envawwwards/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
