7c97ce6a72572699780999b51d10947fad8daca3
layersony <sammaingi5@gmail.com>
2021-07-17 17:51:57 +0300

    userform and profile form add

---
+++ portfolios/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from .models import Projects, Profile, Rate
+from django.contrib.auth.models import User
+
+class UserForm(forms.ModelForm):
+  class Meta:
+    model = User
+    fields = ('username', 'email')
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+      model = Profile
+      fields = '__all__'
+      exclude = ['username', 'count']
+
