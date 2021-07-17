32ed213623277dab2e86c5d88a13d534d15388d8
layersony <sammaingi5@gmail.com>
2021-07-17 13:58:01 +0300

    skeleton for views

--- portfolios/views.py
+++ portfolios/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+  return render(request, 'index.html')
+
+def profile(request):
+  return render(request, 'profile/index.html')
+
+def userprofile(request):
+  return render(request, 'userprofile.html')
+
+def postpoject(request):
+  return render(request, 'profile/postproject.html')
+
+def projectdetails(request):
+  return render(request, 'profile/projectdetails.html')
+
+def search(request):
+  return render(request, 'search.html')
