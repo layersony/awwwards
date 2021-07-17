70d11456304d4667e8d5d3ab2b4e29ebfc4272a4
layersony <sammaingi5@gmail.com>
2021-07-17 14:07:20 +0300

    url patterns  static add

--- portfolios/urls.py
+++ portfolios/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     path('', views.index, name='home'),
     path('account/profile', views.profile, name='profile'),
@@ -9,3 +12,7 @@ urlpatterns = [
     path('projectdetails', views.projectdetails, name='projectdetails'),
     path('search/', views.search, name='search')
 ]
+if settings.DEBUG:
+  urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+  urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+  
