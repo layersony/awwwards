1096a2c045fa35468b1efee2b54cb4d909ece7be
layersony <sammaingi5@gmail.com>
2021-07-17 07:52:07 +0300

    profile model add

--- portfolios/models.py
+++ portfolios/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from tinymce.models import HTMLField
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 # class projects
@@ -15,5 +17,24 @@ class Projects(models.Model):
     return self.title
 
 # class profile
+class Profile(models.Model):
+  profilePic = models.ImageField(upload_to='userProfile/', null=True, blank=True)
+  username = models.OneToOneField(User, on_delete=models.CASCADE)
+  bio = HTMLField(blank=True, null=True)
+  email = models.EmailField(blank=True, null=True)
+  phone = models.IntegerField(blank=True, null=True)
+  count = models.IntegerField(default=0, null=True, blank=True)
+
+  @receiver(post_save, sender=User)
+  def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+      Profile.objects.create(username=instance)
+
+  @receiver(post_save, sender=User)
+  def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+  def __str__(self):
+    return self.username.username
 
 # class reviews
