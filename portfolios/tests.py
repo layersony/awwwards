9cd94fbb8a673b85bbfb21fb35e1a5211e327fcd
layersony <sammaingi5@gmail.com>
2021-07-17 10:59:31 +0300

    project test setup tear down instance

--- portfolios/tests.py
+++ portfolios/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import Projects, Profile, Rate
+from django.contrib.auth.models import User
+
+class TestProjects(TestCase):
+  def setUp(self):
+    self.newproject = Projects(title='Delani Studion', image='test.jpg', projectowner= User.objects.create(username='layersony'), description='a clone for delani studio', livelink='http://google.com/?search=this+is+amazing')
+    self.newproject.save_project()
+
+  def teardown(self):
+    User.objects.all().delete()
+    Projects.objects.all().delete()
+
+  def test_isinstance(self):
+    self.assertTrue(isinstance(self.newproject, Projects))
 
 # Create your tests here.
