fdca32b0b0d610a078640d000b97cbff91216799
layersony <sammaingi5@gmail.com>
2021-07-17 17:52:37 +0300

    profile logic and autoscape on

--- portfolios/views.py
+++ portfolios/views.py
@@ -1,12 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from .models import Profile, Projects, Rate
+from .forms import UserForm, ProfileForm
+from django.contrib import messages
 
 def index(request):
   allprojects = Projects.objects.all()
   return render(request, 'index.html', {'allprojects':allprojects})
 
+@login_required(login_url='/accounts/login/')
 def profile(request):
-  return render(request, 'profile/index.html')
+  if request.method == 'POST':
+    userform = UserForm(request.POST, instance=request.user)
+    profileform = ProfileForm(request.POST, request.FILES, instance=request.user.profile)
+    if userform.is_valid and profileform.is_valid():
+      userform.save()
+      profileform.save()
+      messages.success(request, 'Profile updated successfully')
+    return redirect('profile')
+
+  userform = UserForm()
+  profileform = ProfileForm()
+  curr_profile = Profile.objects.get(username = request.user)
+  curr_projects = Projects.user_projects(request.user)
+  params = {'curr_user': curr_profile, 
+            'curr_project': curr_projects,
+            'userform':userform,
+            'profileform':profileform,
+            }
+  return render(request, 'profile/index.html', params)
 
 def userprofile(request):
   return render(request, 'userprofile.html')
