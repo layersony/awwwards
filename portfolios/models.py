4e2e78337b18df04c68d82bdee45421e22a2a114
layersony <sammaingi5@gmail.com>
2021-07-17 07:45:30 +0300

    project model add

--- portfolios/models.py
+++ portfolios/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
 
-# Create your models here.
+
+# class projects
+class Projects(models.Model):
+  title = models.CharField(max_length=200)
+  image = models.ImageField(upload_to='project/', null=True, blank=True)
+  username = models.OneToOneField(User, on_delete=models.CASCADE)
+  description = HTMLField(null=True, blank=True)
+  livelink = models.URLField(null=True, blank=True)
+
+  def __str__(self):
+    return self.title
+
+# class profile
+
+# class reviews
