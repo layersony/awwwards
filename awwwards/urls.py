8fdb126bd3f6230cb29ddb04f7246c90b92692e7
layersony <sammaingi5@gmail.com>
2021-07-17 16:49:06 +0300

    registration login urls add

--- awwwards/urls.py
+++ awwwards/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
+from django.contrib.auth import views 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('portfolios.urls'))
+    path('', include('portfolios.urls')),
+    path('accounts/', include('registration.backends.simple.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
