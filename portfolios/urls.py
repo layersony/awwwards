1968a1cda350bd3943da6373cd113953aa76afcb
layersony <sammaingi5@gmail.com>
2021-07-17 14:05:42 +0300

    url patterns added

--- portfolios/urls.py
+++ portfolios/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    
+    path('account/profile', views.profile, name='profile'),
+    path('userprofile', views.userprofile, name='userprofile'),
+    path('account/postproject', views.postpoject, name='postproject'),
+    path('projectdetails', views.projectdetails, name='projectdetails'),
+    path('search/', views.search, name='search')
 ]
