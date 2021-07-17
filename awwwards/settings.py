9080de0dc4b8834f63773e7f932b1e16063785ba
layersony <sammaingi5@gmail.com>
2021-07-17 13:19:08 +0300

    admin models add

--- awwwards/settings.py
+++ awwwards/settings.py
@@ -44,7 +44,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'django.contrib.context_processors.media',
+                'django.template.context_processors.media',
             ],
         },
     },
