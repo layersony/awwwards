6b2e0d41317861d9f65312e44ade0ec5e24eb413
layersony <sammaingi5@gmail.com>
2021-07-19 00:09:06 +0300

    more details rate add

--- portfolios/models.py
+++ portfolios/models.py
@@ -5,12 +5,24 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.db.models import Q
 
+rating = (
+    (1, '1'),
+    (2, '2'),
+    (3, '3'),
+    (4, '4'),
+    (5, '5'),
+    (6, '6'),
+    (7, '7'),
+    (8, '8'),
+    (9, '9'),
+    (10, '10'),
+)
 
 # class projects
 class Projects(models.Model):
   title = models.CharField(max_length=200)
   image = models.ImageField(upload_to='project/', null=True, blank=True)
-  projectowner = models.OneToOneField(User, on_delete=models.CASCADE)
+  projectowner = models.ForeignKey(User, on_delete=models.CASCADE)
   description = HTMLField(null=True, blank=True)
   livelink = models.URLField(null=True, blank=True)
 
@@ -38,9 +50,13 @@ class Projects(models.Model):
     allprojects = cls.objects.all()
     return allprojects
 
+  @classmethod
   def searchProjects(cls, searchterm):
-    searchresults = cls.objects.filter(Q(title__icontains=searchterm) | Q(description__icontains=searchterm) | Q(projectowner__icontains=searchterm))
+    searchresults = cls.objects.filter(Q(title__icontains=searchterm) | Q(description__icontains=searchterm) | Q(projectowner__username__icontains=searchterm))
     return searchresults
+
+  class Meta:
+    ordering = ['-id']
 
 # class profile
 class Profile(models.Model):
@@ -64,15 +80,24 @@ class Profile(models.Model):
  
 # class reviews
 class Rate(models.Model):
-  design = models.IntegerField(null=True, blank=True, default=0)
-  usability = models.IntegerField(null=True, blank=True, default=0)
-  content = models.IntegerField(null=True, blank=True, default=0)
+  design = models.IntegerField(choices=rating, null=True, blank=True, default=0)
+  usability = models.IntegerField(choices=rating, null=True, blank=True, default=0)
+  content = models.IntegerField(choices=rating, null=True, blank=True, default=0)
+  design_average = models.FloatField(default=0, blank=True)
+  usability_average = models.FloatField(default=0, blank=True)
+  content_average = models.FloatField(default=0, blank=True)
+  overall = models.FloatField(default=0, blank=True)
   review = models.CharField(max_length=300, blank=True, null=True)
-  project = models.ForeignKey(Projects, on_delete=models.CASCADE)
+  project = models.ForeignKey(Projects, on_delete=models.CASCADE) 
   user = models.ForeignKey(User, on_delete=models.CASCADE)
 
   def __str__(self):
     return self.review[:20]
+
+  @classmethod
+  def get_rates(cls, id):
+    ratings = cls.objects.filter(id=id).all()
+    return ratings
 
   def save_rate(self):
     self.save()
