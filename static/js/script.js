02ca9182cdde207e430278c8913b9c17db37021f
layersony <sammaingi5@gmail.com>
2021-07-19 00:10:10 +0300

    ajax logic add

---
+++ static/js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+  $('#rateforms').submit(function(event){
+    event.preventDefault();
+    form = $('#rateforms')
+    $.ajax({
+      'url': '/ajax/ratereview/', // link
+      'type': 'POST', // method 
+      'data': form.serialize(), // turns from data to json
+      'dataType':'json', // the data type
+      'success': function(data){
+        alert(data['design_average'])
+        $('#id_design').val('');
+        $('#id_usability').val('');
+        $('#id_content').val('');
+        $('#id_review').val('');
+      }
+    })
+  })
+})
