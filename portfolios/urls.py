261ae7c23d5ac8c067792799fa4aa33bc884f975
layersony <sammaingi5@gmail.com>
2021-07-19 00:09:31 +0300

    dynamic linking & ajax add

--- portfolios/urls.py
+++ portfolios/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 from django.conf.urls import include
 from . import views
 
@@ -8,13 +8,14 @@ from django.conf import settings
 urlpatterns = [
     path('', views.index, name='home'),
     path('account/profile', views.profile, name='profile'),
-    path('userprofile', views.userprofile, name='userprofile'),
+    path('userprofile/<int:id>', views.userprofile, name='userprofile'),
     path('account/postproject', views.postpoject, name='postproject'),
-    path('projectdetails', views.projectdetails, name='projectdetails'),
+    path('projectdetails/<int:id>/', views.projectdetails, name='projectdetails'),
     path('search/', views.search, name='search'),
     path('tinymce/', include('tinymce.urls')),
-
+    path('ajax/ratereview/', views.ratereview, name='ratereview')
 ]
+
 if settings.DEBUG:
   urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
   urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
