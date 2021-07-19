16cfc30f511257b1900dda215f4707eaabf85808
layersony <sammaingi5@gmail.com>
2021-07-19 09:21:56 +0300

    deleted files

--- envawwwards/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/layersony/Documents/Canvas_Moringa/Core/Python/django/Week_3/awwwards/envawwwards/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
